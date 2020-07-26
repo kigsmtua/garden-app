b4120573c115890e590a010217b2d9ecbdd8b5c2
John Kiragu <mutuakiragu@gmail.com>
2020-07-26 15:42:31 +0300

    Update settings.py

--- garden/garden/settings.py
+++ garden/garden/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mi*vtxf*amz4(d8mc2&vklh=6qqu+%)darxbg!epgd1@%otnjb'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
